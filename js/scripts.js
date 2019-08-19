d084e967b97643592c28ca583435d49a978164f5
Student <example@example.com>
2019-08-19 14:16:44 -0700

    form now correctly returns user input's in name and Q3 field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("formName").submit(function(event) {
+  $("#formName").submit(function(event) {
     var userName = $("input#name1").val();
     $(".nameSpan").text(userName);
     $("#greetingCreepy").show();
@@ -8,7 +8,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $("formQ3").submit(function(event) {
+  $("#formQ3").submit(function(event) {
     var question3entry = $("input#question3").val();
     $(".resultSpan").text(question3entry);
     $("#decisionTime").show();
